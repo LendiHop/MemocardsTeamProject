524f0b35a196444b8c5eb00fc5b2070dc2ca9f1a
LendiHop <mishin.danil2017@gmail.com>
2021-07-23 18:44:27 +0300

    Error404 page fixed

--- src/n2-features/f0-test/t2-404/Error404.tsx
+++ src/n2-features/f0-test/t2-404/Error404.tsx
@@ -4,19 +4,40 @@ import s from './Error404.module.css';
 export const Error404 = () => {
     return (
         <div className={s.container}>
-            <h3 className={s.h3}>OH JEEZ, IT'S A</h3>
-            <h2 className={s.h2}>4</h2>
-            <h2 className={s.four}>4</h2>
-            <div className={s.morty}>
-                <div className={s.hair}></div>
-                <div className={s.ears}></div>
-                <div className={s.face}></div>
-                <div className={s.eyes}></div>
-                <div className={s.nose}></div>
-                <div className={s.mouth}></div>
-                <div className={s.lines}></div>
+            <div className={s.rail}>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
+                <div className={s.stamp + " " + s.four}>4</div>
+                <div className={s.stamp + " " + s.zero}>0</div>
             </div>
-            <h1 className={s.h1}>ERROR</h1>
+            <div className={s.world}>
+                <div className={s.forward}>
+                    <div className={s.box}>
+                        <div className={s.wall}></div>
+                        <div className={s.wall}></div>
+                        <div className={s.wall}></div>
+                        <div className={s.wall}></div>
+                        <div className={s.wall}></div>
+                        <div className={s.wall}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
